6b5f89a2b32609b773667fbcaf99a0af164c8d4b
Darko Brnović <darko.b@mgsw.local>
2023-07-12 10:10:36 +0200

    created a custom answer button

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basics/answer_button.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -24,22 +25,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
             ),
           ),
           const SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Answer 1'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Answer 2'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Answer 3'),
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Answer 4'),
-          ),
+          AnswerButton(answerText: 'Answer Text 1', onTap: () {}),
+          AnswerButton(answerText: 'Answer Text 2', onTap: () {}),
+          AnswerButton(answerText: 'Answer Text 3', onTap: () {}),
         ],
       ),
     );
