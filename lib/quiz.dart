822cac0d10316211414a3052538e460c9a439b6f
Darko Brnović <darko.b@mgsw.local>
2023-07-12 14:33:34 +0200

    getting started with the results screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:adv_basics/data/questions.dart';
 import 'package:adv_basics/questions_screen.dart';
+import 'package:adv_basics/results_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'package:adv_basics/start_screen.dart';
@@ -29,7 +30,7 @@ class _QuizState extends State<Quiz> {
 
     if (selectedAnswers.length == questions.length) {
       setState(() {
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
         selectedAnswers = [];
       });
     }
@@ -44,6 +45,10 @@ class _QuizState extends State<Quiz> {
         //named argument. forwarding  chooseAnswer to QuestionsScreen
         onSelectAnswer: chooseAnswer,
       );
+    }
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = const ResultstScreen();
     }
 
     return MaterialApp(
