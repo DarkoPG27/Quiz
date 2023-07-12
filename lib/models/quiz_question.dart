8ec51aa846b1f45663448d036900351292e6cb0b
Darko Brnović <darko.b@mgsw.local>
2023-07-12 12:17:43 +0200

    created getShuffledAnswers

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,11 @@ class QuizQuestion {
 
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAnswers() {
+    final shuffledList = List.of(
+        answers); //create the copy of list and stored this copy in shuffledList
+    shuffledList.shuffle(); //then we call shuffle
+    return shuffledList; // then return the updated shuffledList
+  }
 }
